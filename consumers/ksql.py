c5948d439e72024bfb02e1287c8384792ecb29d9
Thomas Andre <thomasandre84@gmail.com>
2020-03-25 12:22:42 +0100

    style: remove TODO and commented out lines
    Remove TODO and commented out lines in ksql.py which could lead to irritation in some cases

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -13,15 +13,13 @@ logger = logging.getLogger(__name__)
 KSQL_URL = "http://localhost:8088"
 
 #
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
+# Complete the following KSQL statements.
+# For the first statement, create a `turnstile` table from your turnstile topic.
 #       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
+# For the second statment, create a `turnstile_summary` table by selecting from the
 #       `turnstile` table and grouping on station_id.
 #       Make sure to cast the COUNT of station id to `count`
 #       Make sure to set the value format to JSON
-
-# topic: com.udacity.turnstiles
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (

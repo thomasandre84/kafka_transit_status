1f1637e6a8e760985012b3a77465859af31d35b6
Thomas Andre <thomasandre84@gmail.com>
2020-03-25 12:19:05 +0100

    style: remove TODO
    Remove TODO in connector.py which has already been implemented

--- producers/connector.py
+++ producers/connector.py
@@ -20,14 +20,7 @@ def configure_connector():
         logging.debug("connector already created skipping recreation")
         return
 
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
-     #   return
-
-    # TODO: Complete the Kafka Connect Config below.
+    # Complete the Kafka Connect Config below.
     # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
     # using incrementing mode, with `stop_id` as the incrementing column name.
     # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
